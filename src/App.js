bacd1061a05a42a4f1a3ef8fe18c47231d52a7d9
Carmen <carmendinu5@yahoo.com>
2020-09-30 15:03:00 +0300

    [master] update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 function App() {
 	const [task, setTask] = useState([]);
 	const [newTask, setNewTask] = useState();
-	const [checked, setChecked] = useState(true);
+	const [checked, setChecked] = useState(false);
 
 	useEffect(() => {
 		fetch(' http://localhost:6789/tasks')
@@ -69,26 +69,26 @@ function App() {
 	};
 	return (
 		<div className="row justify-content-md-center">
-			<div className="card" style={{ width: 400 }}>
+			<div className="card p-3 m-5" style={{ width: 550 }}>
 				<img
 					className="card-img-top"
-					src="http://s3-ap-southeast-1.amazonaws.com/images.humanresourcesonline.net/wp-content/uploads/2014/08/SabrinaZolkifi-Aug-2014-multi-tasking-woman-cartoon-shutterstock.jpg"
+					src="https://images.ctfassets.net/y5z23yb0t4f0/6HoqTFB9Sp7u7WdLBw0mN8/56af5b3f59dd5fc3c57b65bed1125df0/GettyImages-6696383282.jpg"
 					alt="Card"
 				></img>
 				<div className="card-body">
 					<h4 className="card-title">Tasks List</h4>
 
 					<input
-						className="form-control"
+						className=" form-control form-control-lg "
 						placeholder="Enter new task"
 						type="text"
 						onChange={(event) => setNewTask(event.target.value)}
 						onKeyPress={handleKeypress}
 						value={newTask}
 					></input>
-
+					<br></br>
 					<input
-						className="form-control"
+						className="form-control form-control-lg "
 						placeholder="Filter tasks"
 						onChange={(e) =>
 							fetch('http://localhost:6789/tasks/filter/?name=' + e.target.value)
@@ -101,9 +101,10 @@ function App() {
 						}
 						type="text"
 					></input>
+					<br></br>
 					<ul className="list-group">
 						{task.map((item) => (
-							<li className="list-group-item list-group-item-info" key={item.id}>
+							<li className="list-group-item list-group-item-info m-1" key={item.id}>
 								<input
 									onChange={() => checkBox(item.id)}
 									type="checkbox"
@@ -113,7 +114,7 @@ function App() {
 
 								<a href={item.url}>{item.title}</a>
 								<button className="btn btn-outline-success" onClick={() => deleteTask(item.id)}>
-									Delete
+									<img className="btnImg" src="https://img.icons8.com/wired/64/000000/trash.png" />
 								</button>
 							</li>
 						))}
